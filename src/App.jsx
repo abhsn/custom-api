3f12b2d2e36edb57d030209ac006216b9d76acec
Abid Hasan <ab1dh4san@gmail.com>
2023-03-12 21:21:48 +0600

    add table with fake data

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [newUser, setNewUser] = useState(false)
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="w-11/12 mx-auto">
+      <h3 className="text-center font-bold text-4xl my-4">User list</h3>
+      <table className="w-full border-collapse border-2 [&>thead>tr>th]:border-2 [&>tbody>tr>td]:border-2">
+        <thead>
+          <tr>
+            <th>Count</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Website</th>
+          </tr>
+        </thead>
+        <tbody className="[&>tr>td]:text-center">
+          <tr>
+            <td>1</td>
+            <td>Abid Hasan</td>
+            <td>ab1dh4san@gmail.com</td>
+            <td>+8801234567890</td>
+            <td>https://example.com</td>
+          </tr>
+        </tbody>
+      </table>
+      {
+        !newUser ?
+          <button onClick={() => setNewUser(true)} className="bg-blue-500 px-4 py-2 my-4 rounded-md text-white">Add user</button>
+          :
+          <button onClick={() => setNewUser(false)} className="bg-red-500 px-4 py-2 my-4 rounded-md text-white">Cancel</button>
+      }
     </div>
   )
 }
